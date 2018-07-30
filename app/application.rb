a7e779b61aead5cc8650030c501e491d28b5c00a
Richard Fenix <richard.fenix@gmail.com>
2018-07-30 19:55:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
       binding.pry
       item_name = req.path.split("/items/").last #turn /items/Figs into Figs
             item = @@items.find{|s| s.name == item_name}
+            bindning.pry
             resp.write item.price
             resp.status = 200
     else
