bcdb7cc468474a54efd8ba9cb7723f830d7d5f52
Richard Fenix <richard.fenix@gmail.com>
2018-07-30 19:57:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items"
+    if req.path.match("/items/")
       binding.pry
       item_name = req.path.split("/items/").last #turn /items/Figs into Figs
             item = @@items.find{|s| s.name == item_name}
