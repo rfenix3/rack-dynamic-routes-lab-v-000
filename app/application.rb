d1e9026537ab20e53e60479e32f036d911e8cc8b
Richard Fenix <richard.fenix@gmail.com>
2018-07-30 19:43:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,20 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path=="/items/"
-      resp.write "You requested an item"
+      item_name = req.path.split("/items/").last #turn /items/Figs into Figs
+            item = @@items.find{|s| s.name == item_name}
+
+            resp.write item.price
+
     else
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
