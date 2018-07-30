cd02f0e594c9ef7766604cc97cbb797b8c2d5cff
Richard Fenix <richard.fenix@gmail.com>
2018-07-30 19:54:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
       item_name = req.path.split("/items/").last #turn /items/Figs into Figs
             item = @@items.find{|s| s.name == item_name}
             resp.write item.price
+            resp.status = 200
     else
       resp.write "Route not found"
       resp.status = 404
